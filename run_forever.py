fb8e5abe50403c78f2bdc657cacb23dbeb6dcd96
Aswinmcw <azayasankaran@tenstorrent.com>
2025-02-20 14:41:25 +0530

    improvements

--- run_forever.py
+++ run_forever.py
@@ -1,11 +1,9 @@
 import subprocess
 import os
 
-run=True
-while False:
+while True:
     if os.path.exists("stop.txt"):
         print("Stop file detected. Exiting...")
-        run=False
         break
     try:
         subprocess.run(["python3.9", "-m", "on9wordchainbot"])
